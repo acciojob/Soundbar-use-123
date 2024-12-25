7484b3ad4736d9b578a38ba1345e3fe1e24c7cba
use-123 <anmolbejugam@gmail.com>
2024-12-25 11:53:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+// script.js
+let currentAudio = null;
+
+function playSound(soundName) {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+    
+    currentAudio = new Audio(`sounds/${soundName}.mp3`);
+    currentAudio.play();
+}
+
+function stopSound() {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+        currentAudio = null;
+    }
+}
